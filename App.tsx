325976bef1f0e7005455b4d4c6319b3fd46c712a
Wojciech Boman <wojciechboman@Wojciechs-MacBook-Air.local>
2023-08-02 18:19:23 +0200

    Add RNBootSplash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import Router from './src/navigation';
 import { store } from './src/store/store';
+import RNBootSplash from 'react-native-bootsplash';
 
 export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true, duration: 500 });
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <PaperProvider>
